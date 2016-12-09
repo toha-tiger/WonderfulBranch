7a760b28ff0e727778b22f443c31567136a95894
Toha Tiger <toha.tiger@gmail.com>
2016-12-09 14:36:41 +0200

    day(s)?

--- bookmark.js
+++ bookmark.js
@@ -3,7 +3,7 @@ javascript:(function () {
   if (window.getSelection && window.getSelection().toString()) {
     s = window.getSelection().toString();
   } else if ($('#taskTitle h2').length) {
-    s = $('#taskTitle h2').text().replace(/\d+\sdays/, '');
+    s = $('#taskTitle h2').text().replace(/\d+\sday(s)?/, '');
   }
   s = s.replace(/[^a-z0-9\-\#]/gmi, " ").trim().replace(/\s+/g, "_").replace(/_?-_?/g, '-');
   prompt('Branch name:', s);
