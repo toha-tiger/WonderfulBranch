c7d2bcf086dd52f7de1b4e9a1da610248c2bbd7b
Toha Tiger <toha.tiger@gmail.com>
2016-12-22 10:13:52 +0200

    jira support

--- bookmark.js
+++ bookmark.js
@@ -4,6 +4,8 @@ javascript:(function () {
     s = window.getSelection().toString();
   } else if ($('#taskTitle h2').length) {
     s = $('#taskTitle h2').text().replace(/\d+\sday(s)?/, '');
+  } else if ($('a#key-val.issue-link').length && $('#summary-val').length) {
+    s = '' + $('a#key-val.issue-link').text() + ' ' + $('#summary-val').text();
   }
   s = s.replace(/[^a-z0-9\-\#]/gmi, " ").trim().replace(/\s+/g, "_").replace(/_?-_?/g, '-');
   prompt('Branch name:', s);
